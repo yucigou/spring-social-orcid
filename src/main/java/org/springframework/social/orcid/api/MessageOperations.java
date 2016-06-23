59f0747f0714b0ac56b93b9d14e8fe6fe9a68aeb
Yuci Gou <yuci.gou@gmail.com>
2016-06-24 00:23:52 +0100

    New ORCID API for adding works to the ORCID record.

--- src/main/java/org/springframework/social/orcid/api/MessageOperations.java
+++ src/main/java/org/springframework/social/orcid/api/MessageOperations.java
@@ -1,12 +1,25 @@
 package org.springframework.social.orcid.api;
 
+import javax.xml.transform.dom.DOMSource;
+
 import org.springframework.social.orcid.jaxb.beans.OrcidProfile;
 
 /**
+ * ORCID APIs
+ * 
  * @author Yuci Gou
  *
  */
 public interface MessageOperations {
 	OrcidProfile getOrcidProfile();
 	OrcidProfile getOrcidProfile(String orcidId, boolean isPublic);
+		
+	/**
+	 * Add works to the ORCID record
+	 * 
+	 * @param orcidId
+	 * @param document
+	 * @return
+	 */
+	boolean addWorks(String orcidId, DOMSource document);
 }
--- src/main/java/org/springframework/social/orcid/api/impl/MessageOperationsImpl.java
+++ src/main/java/org/springframework/social/orcid/api/impl/MessageOperationsImpl.java
@@ -1,18 +1,26 @@
 package org.springframework.social.orcid.api.impl;
 
 import java.util.Arrays;
+import java.util.List;
+
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.sax.SAXSource;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.xml.SourceHttpMessageConverter;
 import org.springframework.social.orcid.api.MessageOperations;
 import org.springframework.social.orcid.jaxb.beans.OrcidMessage;
 import org.springframework.social.orcid.jaxb.beans.OrcidProfile;
 import org.springframework.social.orcid.jaxb.beans.impl.OrcidMessageImpl;
 import org.springframework.social.orcid.utils.OrcidConfigBroker;
 import org.springframework.util.Assert;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -68,4 +76,51 @@ public class MessageOperationsImpl extends AbstractOrcidOperations implements Me
 		String orcidId = OrcidInfo.getInstance().getOrcid(accessToken);
 		return getOrcidProfile(orcidId, true);
 	}
+
+    @Override
+    public boolean addWorks(String orcidId, DOMSource domSource) {
+        List<HttpMessageConverter<?>> messageConverters = restTemplate.getMessageConverters();
+        boolean gotSourceHttpMessageConverter = false;
+        for (HttpMessageConverter<?> messageConverter: messageConverters) {
+            if (messageConverter instanceof SourceHttpMessageConverter) {
+                gotSourceHttpMessageConverter = true;
+            }
+        }
+        if (!gotSourceHttpMessageConverter) {
+            messageConverters.add(new SourceHttpMessageConverter<DOMSource>());
+            restTemplate.setMessageConverters(messageConverters);
+        }
+        
+        String url = OrcidConfigBroker.getOrcidConfig().getApiUrl();
+        url += orcidId + "/orcid-works";        
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_XML));
+        headers.set("Content-Type", "application/orcid+xml");
+
+        HttpEntity<DOMSource> entity = new HttpEntity<DOMSource>(domSource, headers);
+               
+        ResponseEntity<SAXSource> orcidOutput;
+        try {
+            orcidOutput = restTemplate.exchange(url, HttpMethod.POST, entity, SAXSource.class);
+        } catch (HttpClientErrorException e) {
+            HttpStatus httpStatus = e.getStatusCode();
+            if (HttpStatus.UNAUTHORIZED == httpStatus) {
+                throw e;
+            } else if(HttpStatus.CONFLICT == httpStatus){
+                System.out.println("The queried Orcid Account is locked. Can't retrieve data. Don't worry.");
+                throw e;
+            } else {
+                System.out.println("URL in question: " + url);
+                System.out.println("Exception getting ORCID document: " + e.getMessage());
+                throw e;
+            }
+        } catch (Exception e) {
+            System.out.println("URL in question: " + url);
+            System.out.println("Exception getting ORCID document: " + e.getMessage());
+            throw e;
+        }
+        
+        return orcidOutput != null && orcidOutput.getStatusCode() != null && orcidOutput.getStatusCode().value() == HttpStatus.CREATED.value(); 
+    }
 }
